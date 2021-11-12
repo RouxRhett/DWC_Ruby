3a4124d9dd8ae42de3bd7c0f0ddbd9d586f309b8
RouxRhett <nsch.4871@gmail.com>
2021-11-12 17:12:19 +0900

    6章の分です。

---
+++ DWC_Ruby_chapter6/apple.rb
@@ -0,0 +1,10 @@
+apple = 'Yamanashi'
+#apple = 'Nagano'
+
+if apple == 'Aomori'
+  puts 'このりんごは青森県産です。'
+elsif apple == 'Nagano'
+  puts 'このりんごは青森県産ではなく、長野県産です。'
+else
+  puts 'このりんごは青森県産でも長野県産でもありません。'
+end
