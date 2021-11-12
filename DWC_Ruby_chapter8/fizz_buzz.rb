65b86f93a0297ed4c7be6ea7a34f6e124377770d
RouxRhett <nsch.4871@gmail.com>
2021-11-12 21:50:08 +0900

    8章の分です。入力値の制限を設けました。

--- DWC_Ruby_chapter8/fizz_buzz.rb
+++ DWC_Ruby_chapter8/fizz_buzz.rb
@@ -11,10 +11,19 @@ def fizz_buzz(num)
 end
 
 puts "数字を入力してください"
-input_num = gets.to_i
-if input_num != 0
-  puts "結果は..."
-  fizz_buzz(input_num)
-else
-  puts "0以上の数値を入力してください。"
+
+while true do
+  input_num = gets
+  if input_num =~ /^[0-9]+$/
+    input_num = input_num.to_i
+    if input_num != 0
+      puts "結果は..."
+      fizz_buzz(input_num)
+      break
+    elsif input_num == 0
+      puts "0より大きい数値を入力してください。"
+    end
+  else
+    puts '文字列を含まない自然数を入力してください。'
+  end
 end
